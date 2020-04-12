3e15119fd14d5d040597c117a9bcb797a973179d
Fernanda <kastrupf@gmail.com>
2020-04-12 15:15:04 +0200

    Test d'insertion du premier produit dans la base de données.

--- src/test/java/com/mello/ecommerce/startingwithjpa/OperationsAvecTransactionTest.java
+++ src/test/java/com/mello/ecommerce/startingwithjpa/OperationsAvecTransactionTest.java
@@ -1,9 +1,32 @@
 package com.mello.ecommerce.startingwithjpa;
 
 import com.mello.ecommerce.EntityManagerTest;
+import com.mello.ecommerce.model.Produit;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class OperationsAvecTransactionTest extends EntityManagerTest {
+
+    @Test
+    public void insertionDuPremmierProduit() {
+        Produit produit = new Produit();
+
+        produit.setId(2);
+        produit.setLibelle("Câmera Canon");
+        produit.setDescription("A melhor definição para suas fotos.");
+        produit.setPrix(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produit);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produit produitVerification = entityManager.find(Produit.class, produit.getId());
+        Assert.assertNotNull(produitVerification);
+    }
 
     @Test
     public void ouvrirEtFermerLaTransaction() {
