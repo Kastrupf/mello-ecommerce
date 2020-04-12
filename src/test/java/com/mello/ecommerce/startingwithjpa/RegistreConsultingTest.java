cde7ec201fc10d6342a3318dcf2b826340c55918
Fernanda <kastrupf@gmail.com>
2020-04-12 14:45:42 +0200

    Création d'une classe génerique de tests pour les méthodes de callback

--- src/test/java/com/mello/ecommerce/startingwithjpa/RegistreConsultingTest.java
+++ src/test/java/com/mello/ecommerce/startingwithjpa/RegistreConsultingTest.java
@@ -1,38 +1,11 @@
 package com.mello.ecommerce.startingwithjpa;
 
+import com.mello.ecommerce.EntityManagerTest;
 import com.mello.ecommerce.model.Produit;
 import org.junit.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
-public class RegistreConsultingTest {
-
-    private static EntityManagerFactory entityManagerFactory;
-
-    private EntityManager entityManager;
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence
-                .createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
-    }
-
-    @Before
-    public void setUp() {
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @After
-    public void tearDown() {
-        entityManager.close();
-    }
+public class RegistreConsultingTest extends EntityManagerTest {
 
     @Test
     public void chercherParIdentifiant() {
